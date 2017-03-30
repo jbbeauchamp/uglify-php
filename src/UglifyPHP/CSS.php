2ae45cbf9f9c471fc24895c9d372069a5d713eaa
thminhVN <thminhkg@gmail.com>
2017-03-30 17:27:46 +0700

    Fix error check version in CSS

--- src/UglifyPHP/CSS.php
+++ src/UglifyPHP/CSS.php
@@ -5,7 +5,7 @@ namespace UglifyPHP;
 class CSS extends Uglify
 {
     protected static $location = 'cleancss';
-    protected static $exists_check = '';
+    protected static $exists_check = ' -v';
     protected static $options = array('keep-line-breaks', 'compatibility', 'skip-import', 'timeout', 'rounding-precision', 's0');
     protected static $option_place = 'before';
 }
